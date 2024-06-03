2ac78eed923bf96bb4f0415793620ad42a296376
shivam-apptware <shivam.sharma@apptware.com>
2024-06-03 17:33:47 +0530

    User-Authentication and access control

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { useAuth0 } from "@auth0/auth0-react";
+import userData from "./user.json";
 
 function App() {
+  const { user, loginWithRedirect, isAuthenticated, logout } = useAuth0();
+  const [userRole, setUserRole] = useState(null);
+
+  useEffect(() => {
+    if (isAuthenticated && user) {
+      const currentUser = userData.find(
+        (userData) => userData.email === user.email
+      );
+      if (currentUser) {
+        setUserRole(currentUser.role);
+      }
+    } else {
+      setUserRole(null);
+    }
+  }, [isAuthenticated, user]);
+
+  console.log("current user", user);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>User Authentication Application</h1>
+        {isAuthenticated && (
+          <h3>
+            Hello, {user.name} <br></br>
+            <br></br>
+            This Dashboard for {userRole}
+          </h3>
+        )}
+        {isAuthenticated ? (
+          <button onClick={() => logout()}>Logout</button>
+        ) : (
+          <button onClick={() => loginWithRedirect()}>Login</button>
+        )}
       </header>
     </div>
   );
